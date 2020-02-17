65d7cb495b9eedf71b7c138a72d810a61bb9fd08
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-16 22:21:24 -0500

    Adds first Rectangle.java class to the project

---
+++ src/content/Rectangle.java
@@ -0,0 +1,57 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package content;
+
+/**
+ *
+ * @author Dell
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    /**
+     * @return the length
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * @param length the length to set
+     */
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    /**
+     * @return the width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double calculateArea(){
+        return length * width;
+    }
+    public double calculateArea(double length, double width){
+        return length * width;
+    }
+}
---
+++ src/demorectangle1/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package demorectangle1;
+
+import content.Rectangle;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Rectangle r1 = new Rectangle(10,5);
+        System.out.println("Area is : " + r1.calculateArea());
+    }
+    
+}
