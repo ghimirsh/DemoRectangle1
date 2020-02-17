0134b89859e9518efd9ef4641bf8f73ed1fffb93
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-16 23:03:15 -0500

    Adds Circle.java class

---
+++ src/content/Circle.java
@@ -0,0 +1,15 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package content;
+
+/**
+ *
+ * @author Dell
+ */
+public class Circle {
+    private double radius;
+    
+}
